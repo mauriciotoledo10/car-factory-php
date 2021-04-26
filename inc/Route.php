b6e731bb89499f5fa0b1d1b8e28d4f6d0440197b
root <root@ubuntu-s-1vcpu-1gb-intel-sfo3-01>
2021-04-26 19:40:02 +0000

    método add na classe route

--- inc/Route.php
+++ inc/Route.php
@@ -5,4 +5,13 @@ class Route {
     private static $routes = Array();
     private static $pathNotFound = null;
     private static $methodNotAllowed = null;
+
+    public static function add($expression, $function, $method = 'get')
+    {
+        array_push(self::$routes,Array(
+          'expression' => $expression,
+          'function' => $function,
+          'method' => $method
+        ));
+    }
 }
