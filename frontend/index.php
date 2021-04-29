b5a5f20ecd123dc5f0fa0e0845419efa183e3671
root <root@ubuntu-s-1vcpu-1gb-intel-sfo3-01>
2021-04-29 04:16:44 +0000

    ajuste identação

--- frontend/index.php
+++ frontend/index.php
@@ -36,11 +36,11 @@
 
        <!-- Modal para criação de carro -->
     
-       <div class="modal fade" id="create-car" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+       <div class="modal fade" id="create-car" tabindex="-1" role="dialog" aria-labelledby="labelModal">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Create Car</h4>
+                <h4 class="modal-title" id="labelModal">Create Car</h4>
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               </div>
